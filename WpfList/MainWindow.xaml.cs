d6f0e4fb10b89a9ca140940a2c50e7b4c223de33
patrickteivas <teivas.2011@mail.ru>
2018-03-06 09:04:48 +0200

    Commit

--- WpfList/MainWindow.xaml.cs
+++ WpfList/MainWindow.xaml.cs
@@ -32,6 +32,51 @@ namespace WpfList
 
             TodoListBox.ItemsSource = items;
         }
+
+        private void todoListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Title = (TodoListBox.SelectedItem as TodoItem).Title;
+        }
+
+        private void btnShowSelectedItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (TodoListBox.SelectedItem != null)
+                MessageBox.Show((TodoListBox.SelectedItem as TodoItem).Title);
+        }
+
+        private void btnSelectLast_Click (object sender, RoutedEventArgs e)
+        {
+            TodoListBox.SelectedIndex = TodoListBox.Items.Count - 1;
+        }
+
+        private void btnSelectNext_Click(object sender, RoutedEventArgs e)
+        {
+            int nextIndex = 0;
+            if ((TodoListBox.SelectedIndex >= 0) && (TodoListBox.SelectedIndex < (TodoListBox.Items.Count - 1)))
+            {
+                nextIndex = TodoListBox.SelectedIndex + 1;
+            }
+            TodoListBox.SelectedIndex = nextIndex;
+        }
+
+        private void btnSelectCSharp_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in TodoListBox.Items)
+            {
+                if ((item is TodoItem) && ((item as TodoItem).Title.Contains("C#")))
+                {
+                    TodoListBox.SelectedItem = item;
+                }
+            }
+        }
+
+        private void btnSelectAll_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in TodoListBox.Items)
+            {
+                TodoListBox.SelectedItems.Add(item);
+            }
+        }
     }
 
     public class TodoItem
